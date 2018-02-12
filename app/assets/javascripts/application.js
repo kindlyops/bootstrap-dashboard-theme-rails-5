256672a360731050226d81858f2e321fc129b259
Harry Whelchel <harry@standardcraft.io>
2018-02-11 21:55:26 -0500

    reset js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require jquery_ujs
-//= require popper
-//= require bootstrap
+//= require rails_ujs
+//= require turbolinks
 //= require_tree .
